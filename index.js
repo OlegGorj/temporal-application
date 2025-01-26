b83a41328ecd46de5443d24a514c347bec124620
oleggorj <oleggorj@hotmail.com>
2025-01-26 12:57:15 -0500

    fix: update workflow

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 module.exports = {
-    "env": {
-        "node": true,
-        "es2021": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "rules": {
-        "indent": ["error", 4],
-        "semi": ["error", "always"]
-    }
+  "env": {
+    "node": true,
+    "es2021": true
+  },
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    "ecmaVersion": "latest",
+    "sourceType": "module"
+  },
+  "rules": {
+    "indent": ["error", 2],
+    "semi": ["error", "always"],
+    "quotes": ["error", "double"]
+  }
 }
 
 function main() {
