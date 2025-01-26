b3fffdfe00346b05155d1bce849a724e4ad46c29
oleggorj <oleggorj@hotmail.com>
2025-01-26 13:25:21 -0500

    fix: update workflow

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,25 @@
 module.exports = {
-    'env': {
-      'node': true,
-      'es2021': true,
-      'jest': true
-    },
-    'extends': 'eslint:recommended',
-    'parserOptions': {
-      'ecmaVersion': 'latest',
-      'sourceType': 'module'
-    },
-    'rules': {
-      'indent': ['error', 2],
-      'semi': ['error', 'always'],
-      'quotes': ['error', 'single']
-    },
-    'globals': {
-      'describe': 'readonly',
-      'test': 'readonly',
-      'expect': 'readonly',
-      'beforeEach': 'readonly',
-      'afterEach': 'readonly',
-      'jest': 'readonly'
-    }
-  };
-  
+  'env': {
+    'node': true,
+    'es2021': true,
+    'jest': true
+  },
+  'extends': 'eslint:recommended',
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  'rules': {
+    'indent': ['error', 2],
+    'semi': ['error', 'always'],
+    'quotes': ['error', 'single']
+  },
+  'globals': {
+    'describe': 'readonly',
+    'test': 'readonly',
+    'expect': 'readonly',
+    'beforeEach': 'readonly',
+    'afterEach': 'readonly',
+    'jest': 'readonly'
+  }
+};
